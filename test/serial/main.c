e0f47da92de3a8ca4d2a2db5393da962a27baf20
FlorianEpple <florian.epple@gmx.net>
2023-11-26 19:05:53 +0100

    unite at /test/serial to cross platformed

--- test/serial/main.c
+++ test/serial/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#elif defined(__APPLE__)
 #include <string.h>
 #include <dirent.h>
+#endif
 
+void listSerialPorts();
+
+#ifdef _WIN32
+void listSerialPorts()
+{
+    char portName[10];
+    for (int i = 1; i <= 256; ++i)
+    {
+        sprintf(portName, "COM%d", i);
+        HANDLE hSerial = CreateFile(portName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+        if (hSerial != INVALID_HANDLE_VALUE)
+        {
+            printf("Arduino is connected to port: %s\n", portName);
+            CloseHandle(hSerial);
+        }
+    }
+}
+#elif defined(__APPLE__)
 void listSerialPorts()
 {
     DIR *dir;
@@ -23,6 +46,7 @@ void listSerialPorts()
         closedir(dir);
     }
 }
+#endif
 
 int main()
 {
